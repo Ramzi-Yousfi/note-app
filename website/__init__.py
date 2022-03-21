8d263c9718c970f26903d1366f99cdfcf19a7006
RAMZI <ramzi446@hotmail.com>
2022-03-21 12:31:50 +0100

    21/03/2022

---
+++ website/__init__.py
@@ -0,0 +1,13 @@
+
+from . import views
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'zfhzzhiffihzfpoizhpif'
+
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
+    return app
